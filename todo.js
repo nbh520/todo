7a0ddef5df235077846db13db0267379067c6197
nibohan <154524331@qq.com>
2018-09-07 08:45:55 +0800

    更新bug

--- todo.js
+++ todo.js
@@ -6,7 +6,8 @@ const vm = new Vue({
             {isSelected: false, title: '吃饭'}
         ],
         title: '',
-        cur: ''
+        cur: '',
+        hash: ''
     },
     methods: {
         add(){
@@ -27,8 +28,15 @@ const vm = new Vue({
         }
     },
     computed:{
+        filterTodos(){
+            if(this.hash === 'all') return this.todos;
+            if(this.hash === 'finish') return this.todos.filter(item => item.isSelected);
+            if(this.hash === 'unfinish') return this.todos.filter(item => !item.isSelected)
+            return this.todos;
+        },
         count(){
             return this.todos.filter(item => !item.isSelected).length;
+            
         }
     },
     directives:{ //自定义
@@ -39,7 +47,13 @@ const vm = new Vue({
         }
     },
     created(){ //ajax获取 初始化数据
-        this.todos = JSON.parse(localStorage.getItem('data')).length || this.todos;
+        this.todos = JSON.parse(localStorage.getItem('data')) || this.todos;
+        //监控hash值的变化,如果页面以及有hash了， 重新刷新页面也要获取hash值 
+        this.hash = window.location.hash.slice(2) || 'all';
+        window.addEventListener('hashchange', () => {
+            //当hash值变化 重新操作记录
+            this.hash = window.location.hash.slice(2);
+        }, false)
     },
     watch: {
         todos:{ //watch默认只监控一层的数据变化，深度监控
